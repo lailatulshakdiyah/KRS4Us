09801e11eff16cd54067579fa391b5b3f53985c2
khaliladha11 <khaliladha1123@gmail.com>
2024-05-28 16:17:32 +0700

    menambahkan hover

--- resources/js/Components/Calendar.jsx
+++ resources/js/Components/Calendar.jsx
@@ -16,21 +16,26 @@ export default function Calendar({ courses }){
                         <div className="flex flex-col w-1/6 p-2 gap-2">
                             {courses && courses[day].map((course) => {
                                 let bg;
+                                let hoverBg;
                                 switch (course['type']) {
                                     case 'kuliah':
                                         bg = 'bg-primary-300';
+                                        hoverBg = 'hover:bg-primary-400';
                                         break;
                                     case 'praktikum':
                                         bg = 'bg-success-300';
+                                        hoverBg = 'hover:bg-success-400';
                                         break;
                                     case 'responsi':
                                         bg = 'bg-warning-300';
+                                        hoverBg = 'hover:bg-warning-400';
                                         break;
                                     default:
                                         bg = 'bg-gray-75';
+                                        hoverBg = 'hover:bg-gray-100';
                                 }
                                 return (
-                                    <div className={`flex flex-col ${bg} p-1.5 text-white-300 rounded-sm shadow-md`} style={{fontSize: '0.6rem', lineHeight: '0.75rem'}}>
+                                    <div className={`flex flex-col ${bg} ${hoverBg} p-1.5 text-white-300 rounded-sm shadow-md transition duration-300 ease-in-out`} style={{fontSize: '0.6rem', lineHeight: '0.75rem'}}>
                                         <div className="flex font-semibold">{course['start_time']} - {course['end_time']}</div>
                                         <div className="flex">{course['name']}</div>
                                         <div className="flex">{course['type'][0].toUpperCase()}{course['class']} - {course['room']}</div>
