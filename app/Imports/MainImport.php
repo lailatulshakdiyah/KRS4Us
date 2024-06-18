29fc2b4412a07f706dd7419e890930c642cd7558
zran147 <muhammadzahran@apps.ipb.ac.id>
2024-06-18 23:00:18 +0700

    debugging

--- app/Imports/MainImport.php
+++ app/Imports/MainImport.php
@@ -20,6 +20,8 @@ class MainImport implements WithMultipleSheets
             // '*' => new CourseUserImport()
         ];
 
+        dd($this->sheetCount);
+
         for ($i = 2; $i < $this->sheetCount; $i++) {
             $all[$i] = new CourseUserImport();
         }
